639313d59b7a2c0e18cc48a5f23e820bb766bdc6
Avinash Badloe <avinash.badloe@outlook.com>
2020-03-30 12:00:39 +0200

    Remove ignored files

--- BikeRentalPractise/ViewModel/MainWindowViewModel.cs
+++ BikeRentalPractise/ViewModel/MainWindowViewModel.cs
@@ -20,5 +20,39 @@ namespace BikeRentalPractise.ViewModel
             Stores = _db.Stores.Local;
             Bikes = _db.Bikes.Local;
         }
+
+        // Shows the Edit Stores Window
+        private void ShowEditStoresWindow(object sender, RoutedEventArgs e)
+        {
+            MainWindowViewModel vm = (MainWindowViewModel)DataContext;
+
+            EditStoresViewModel editVM = new EditStoresViewModel(vm.Stores);
+
+            EditStoresWindow view = new EditStoresWindow(vm.Stores);
+            view.DataContext = editVM;
+            view.Show();
+        }
+
+        // Shows the Edit Bikes Window
+        private void ShowEditBikesWindow(object sender, RoutedEventArgs e)
+        {
+
+            MainWindowViewModel vm = (MainWindowViewModel)DataContext;
+
+            // Checks if a Store is selected in order to show to Edit Bikes Window
+            if (vm.SelectedStore != null)
+            {
+                EditBikesViewModel editVM = new EditBikesViewModel(vm.SelectedStore.Bikes);
+                EditBikesWindow view = new EditBikesWindow(vm.Bikes);
+
+                view.DataContext = editVM;
+                view.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please select a bike first");
+
+            }
+        }
     }
 }
