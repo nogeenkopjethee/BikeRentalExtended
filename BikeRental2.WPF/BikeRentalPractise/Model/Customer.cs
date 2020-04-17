be46640ac5afeec6d41b37661cc29d8f4a15fd87
Mike Ebbe <s1131313@student.windesheim.nl>
2020-04-17 15:55:49 +0200

    annotations customer

--- BikeRental2.WPF/BikeRentalPractise/Model/Customer.cs
+++ BikeRental2.WPF/BikeRentalPractise/Model/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,16 @@ namespace BikeRentalPractise.Model
 {
     public class Customer
     {
+        [Index(IsUnique = true)]
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public Gender Gender { get; set; }
         public int Height { get; set; }
+        [Required]
         public string Email { get; set; }
     }
 
