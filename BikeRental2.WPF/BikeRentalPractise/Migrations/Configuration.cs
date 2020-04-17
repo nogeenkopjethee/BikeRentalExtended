e06cc69d5caa0475293d0fb92f4f535dde2e5a1a
Naomie Lepelblad <S1136485@student.windesheim.nl>
2020-04-17 15:41:56 +0200

    seed data toegevoegd

--- BikeRental2.WPF/BikeRentalPractise/Migrations/Configuration.cs
+++ BikeRental2.WPF/BikeRentalPractise/Migrations/Configuration.cs
@@ -18,29 +18,187 @@ namespace BikeRentalPractise.Migrations
         protected override void Seed(BikeRentalPractise.Model.BikeStoreModel context)
         {
             context.Stores.AddOrUpdate(x => x.Name,
-                new Store()
+                new Store
                 {
                     Name = "Beste Fietsenwinkel in Almere",
+                    Id = 1,
+                    Address = "",
+                    City = "Almere",
+                    MaxCapacity = 50,
                     Bikes = new ObservableCollection<Bike>
                     {
                         new Bike()
                         {
                             BikeModel = BikeModel.Batavus,
-                        }
+                            BikeType  = BikeType.EBike,
+                            BikeGender = BikeGender.Man,
+                            Size = 100,
+                            HourlyRate = 5,
+                            DailyRate =  50,
+                        },
+
+                        new Bike()
+                        {
+                            BikeModel = BikeModel.Altec ,
+                            BikeType  = BikeType.Mountainbike,
+                            BikeGender = BikeGender.Man,
+                            Size = 70,
+                            HourlyRate = 6,
+                            DailyRate =  60,
+                        },
+
+                        new Bike()
+                        {
+
+                            BikeModel = BikeModel.Bellage ,
+                            BikeType  = BikeType.Racefiets,
+                            BikeGender = BikeGender.Vrouw,
+                            Size = 80,
+                            HourlyRate = 5,
+                            DailyRate =  50,
+                        },
+
+                         new Bike()
+                        {
+
+                            BikeModel = BikeModel.Spirit,
+                            BikeType  = BikeType.Stadsfiets,
+                            BikeGender = BikeGender.Vrouw,
+                            Size = 85,
+                            HourlyRate = 6,
+                            DailyRate =  60,
+                        },
+                         new Bike()
+                         {
+                            BikeModel = BikeModel.Popal,
+                            BikeType  = BikeType.Kinderfiets,
+                            BikeGender = BikeGender.Man,
+                            Size = 40,
+                            HourlyRate = 2,
+                            DailyRate = 20,
+                         },
+
+                         new Bike()
+                         {
+                            BikeModel = BikeModel.Union,
+                            BikeType  = BikeType.Kinderfiets,
+                            BikeGender = BikeGender.Vrouw,
+                            Size = 40,
+                            HourlyRate = 2,
+                            DailyRate = 20,
+                         }
                     }
                 },
-                new Store()
-            {
-                Name = "Beste Fietsenwinkel in Almere",
-                Bikes = new ObservableCollection<Bike>
+
+                new Store
                 {
+                    Name = "Beste Fietsenwinkel in Amsterdam",
+                    Id = 2,
+                    Address = "",
+                    City = "Amsterdam",
+                    MaxCapacity = 100,
+                    Bikes = new ObservableCollection<Bike>
+                    {
+
                     new Bike()
                     {
                         BikeModel = BikeModel.Batavus,
+                    },
+
+                    new Bike()
+                    {
+                        BikeModel = BikeModel.Umit,
+                        BikeType  = BikeType.Omafiets,
+                        BikeGender = BikeGender.Man,
+                        Size = 70,
+                        HourlyRate = 3,
+                        DailyRate =  30,
+                    },
+
+                        new Bike()
+                        {
+                            BikeModel = BikeModel.Gazelle ,
+                            BikeType  = BikeType.Driewieler,
+                            BikeGender = BikeGender.Man,
+                            Size = 20,
+                            HourlyRate = 2,
+                            DailyRate =  20,
+                        },
+
+                        new Bike()
+                        {
+
+                            BikeModel = BikeModel.Sensa ,
+                            BikeType  = BikeType.Racefiets,
+                            BikeGender = BikeGender.Vrouw,
+                            Size = 70,
+                            HourlyRate = 5,
+                            DailyRate =  50,
+                        },
+
+                         new Bike()
+                        {
+
+                            BikeModel = BikeModel.Spirit,
+                            BikeType  = BikeType.Mountainbike,
+                            BikeGender = BikeGender.Vrouw,
+                            Size = 85,
+                            HourlyRate = 6,
+                            DailyRate =  60,
+                        },
+                         new Bike()
+                         {
+                            BikeModel = BikeModel.Popal,
+                            BikeType  = BikeType.Kinderfiets,
+                            BikeGender = BikeGender.Man,
+                            Size = 40,
+                            HourlyRate = 2,
+                            DailyRate = 20,
+                         },
+
+                         new Bike()
+                         {
+                            BikeModel = BikeModel.Cortina,
+                            BikeType  = BikeType.Kinderfiets,
+                            BikeGender = BikeGender.Vrouw,
+                            Size = 40,
+                            HourlyRate = 2,
+                            DailyRate = 20,
+                         }
                     }
-                }
-            });
+                });
+            context.Customers.AddOrUpdate(x => x.Id,
+                new Customer
+                {
+                    Id = 11,
+                    FirstName = "Geralt",
+                    LastName = "Rivia",
+                    Gender = Gender.Man,
+                    Height = 188,
+                    Email = "geralt@test.nl",
+                },
+                 new Customer
+                 {
+                     Id = 22,
+                     FirstName = "Nienke",
+                     LastName = "Meijer",
+                     Gender = Gender.Vrouw,
+                     Height = 178,
+                     Email = "nienke@test.nl",
+                 },
+                  new Customer
+                  {
+                      Id = 33,
+                      FirstName = "Jenny",
+                      LastName = "Blok",
+                      Gender = Gender.Overig,
+                      Height = 166,
+                      Email = "jenny@test.nl",
+                  }
+              );
 
         }
+                
+   
     }
 }
